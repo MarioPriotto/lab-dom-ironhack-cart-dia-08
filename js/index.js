ee48a8237065cbc4f008336eeaa64d413b7ae73d
Mario Cezar Priotto <mariopriotto@gmail.com>
2022-10-25 22:39:40 -0300

    Versao 01

--- js/index.js
+++ js/index.js
@@ -2,41 +2,71 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price    = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.quantity input').value;
+  const subtotal = product.querySelector('.subtotal span').textContent = price * quantity;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let totalGeral = 0;
+  [...document.querySelectorAll('.product')].forEach(e => {
+        updateSubtotal(e);
+        totalGeral += parseInt(e.querySelector('.subtotal span').textContent);
+      });
+ 
+  document.querySelector('#total-value span').textContent = totalGeral;
 
-  // ITERATION 3
-  //... your code goes here
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const vo  = target.parentNode.parentNode;
+  const bi  = target.parentNode.parentNode.parentNode;
+  bi.removeChild(vo);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
+  let clone = document.querySelector('.product').cloneNode(true);
+  clone.querySelector('.name  span').textContent = document.querySelectorAll('.create-product td input')[0].value;
+  clone.querySelector('.price span').textContent = document.querySelectorAll('.create-product td input')[1].value;
+  clone.querySelector('.quantity input').value   = 0;
+  
+  let destino = document.querySelector('body table');
+  destino.appendChild(clone);
+
+  [...document.querySelectorAll('.btn-remove')].forEach(e => {
+     e.onclick = function (event) {
+        removeProduct(event);
+     }
+  });
+
+  calculateAll();
+  
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  [...document.querySelectorAll('.btn-remove')].forEach(e => {
+    e.onclick = function (event) {
+        removeProduct(event);
+    }
+  });
+
+  document.getElementById('create').addEventListener('click',() => { createProduct() } );
+
 });
